e99e0c987bfb54fed3d7e9c0cb6e1df83d3b6e85
bobbymessiah <collier.bobby@yahoo.com>
2015-12-05 11:06:06 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ return(num1 / num2)
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
+def greeting(name, age)
+  puts "Hi my name is #{name}. I am #{age} years old."
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(pie="cheese")
+  pie
 end
